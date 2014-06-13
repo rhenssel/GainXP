c622b6c02e2b96e45b31dcb06366782a0847931f
Rune Henssel <rune@henssel.dk>
2014-06-13 13:38:22 +0200

    * Started to add support for user login

--- gain_xp/lib/GainXP.pm
+++ gain_xp/lib/GainXP.pm
@@ -3,16 +3,61 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+	my $self = shift;
+	
+	
+    $self->{secrets} = ['DUMMY']; # Keeps Mojolicious happy until the real secrets can be loaded from the config file.
+    # Loading configuration and applying app settings
+    $self->plugin('Config' => {file => $self->app->home->rel_file('Config/GainXP.conf')});
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    $self->plugin('bcrypt', { cost => $self->{config}->{Security}->{BCryptCost} });
 
-  # Router
-  my $r = $self->routes;
+	$self->plugin('database', {
+		dsn      => $self->{config}->{DataBase}->{ConnectString},
+		username => $self->{config}->{DataBase}->{User},
+		password => $self->{config}->{DataBase}->{Password},
+		options  => { PrintError => 0,
+			RaiseError => 1,
+			AutoCommit => 1,
+			LongReadLen => 64 * 1024,
+			LongTruncOk => 1,
+			Callbacks => { 'connect_cached.reused' => sub { delete $_[4]->{AutoCommit} },
+			},
+		},
+		helper   => 'db',
+		});
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+	# Documentation browser under "/perldoc"
+	$self->plugin('PODRenderer');
+
+	# Router
+	my $r = $self->routes;
+
+	# Normal route to controller
+	$r->get('/')->to('example#welcome');
+  
+	my $rs = $r->bridge("/")->to(cb => sub {
+		my $self = shift;
+
+		# Authenticated
+		if($self->session('auth')) {
+			return 1;
+		}
+
+		my $encodedReferer = b64_encode($self->req->url(),'');
+
+		my $checksum = hmac_sha1_sum($encodedReferer, $self->app->secrets->[0]);
+
+		# Not authenticated
+		$self->redirect_to('/login/?ref='.b64_encode($self->req->url(),'').'::'.$checksum);
+		return 0;
+	});
+	
+	$r->route('/login/')                                                                     ->via('get')   ->to('Login#login_form');
+    $r->route('/login/')                                                                     ->via('post')  ->to('Login#login');
+
+	$rs->route('/logout/')                                                                   ->via('get')   ->to('Login#logout');
+	
 }
 
 1;
