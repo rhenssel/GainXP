5a4afc5a01838acfbadce1194c6c403ed1f1c945
Florian LE-BACHELIER-DE-LA-RIVIERE <florian.dlr@gmail.com>
2014-06-13 17:12:30 +0200

    register ok

--- gain_xp/lib/GainXP.pm
+++ gain_xp/lib/GainXP.pm
@@ -56,7 +56,7 @@ sub startup {
 	});
 	
 	$r->route('/login/')->via('get')->to('Login#login_form');
-	$r->route('/register/')->via('get')->to('Register#register_form');
+	$r->route('/register')->via('get')->to('Register#register_form');
 	$r->route('/login/')->via('post')->to('Login#login');
 	$rs->route('/logout/')->via('get')->to('Login#logout');
 	
