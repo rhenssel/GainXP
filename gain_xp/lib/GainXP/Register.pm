5b1f2f220c1d01c6a636ae51e2722eb3d7611888
Florian LE-BACHELIER-DE-LA-RIVIERE <florian.dlr@gmail.com>
2014-06-14 11:30:25 +0200

    verif pass-login-email

--- gain_xp/lib/GainXP/Register.pm
+++ gain_xp/lib/GainXP/Register.pm
@@ -1,5 +1,8 @@
 package GainXP::Register;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojolicious::Sessions;
+use DBI;
+use Digest;
 
 sub register_handler {
 	my $self = shift;
@@ -8,7 +11,7 @@ sub register_handler {
 	my $confirm_password = $self->param('confirm_password');
 	my $email = $self->param('email');
 	if ($password != $confirm_password) {
-		$self->stash(invalidRegister => 1);
+		$self->stash(invalidPassword => 1);
 		return $self->render(template => 'Register/register_form', layout => 'default', title => 'Register Fail');
 	}
 	return $self->render(template => 'Register/register_form', layout => 'default', title => 'Register ok');
